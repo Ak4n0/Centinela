e83480330c5b5481fa3bdafbdcdf9f00b45eff0f
'Miquel' <'miquelfs1981@gmail.com'>
2020-03-25 00:19:33 +0100

    Añadida clase para el pojo del token de actualización de contarseña

---
+++ src/modelo/pojo/TokenNuevoPassword.java
@@ -0,0 +1,54 @@
+package modelo.pojo;
+
+import java.util.Date;
+
+public class TokenNuevoPassword {
+	private String id;
+	private Integer idUsuario;
+	private Date fechaHora;
+	
+	public TokenNuevoPassword() {}
+
+	/**
+	 * @return the id
+	 */
+	public String getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the idUsuario
+	 */
+	public Integer getIdUsuario() {
+		return idUsuario;
+	}
+
+	/**
+	 * @param idUsuario the idUsuario to set
+	 */
+	public void setIdUsuario(Integer idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+
+	/**
+	 * @return the fechaHora
+	 */
+	public Date getFechaHora() {
+		return fechaHora;
+	}
+
+	/**
+	 * @param fechaHora the fechaHora to set
+	 */
+	public void setFechaHora(Date fechaHora) {
+		this.fechaHora = fechaHora;
+	}
+	
+}
