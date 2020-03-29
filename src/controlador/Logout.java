e00021448028d462567f990bd0fd813a0ed96106
'Miquel' <'miquelfs1981@gmail.com'>
2020-03-29 21:35:25 +0200

    Añadido logout del usuario

---
+++ src/controlador/Logout.java
@@ -0,0 +1,33 @@
+package controlador;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Logout
+ */
+@WebServlet("/Logout")
+public class Logout extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// Elimina la sesión del usuario
+		HttpSession session = request.getSession(false);
+		
+		if(session != null) {
+			session.invalidate();
+		}
+		
+		// Redirigimos a la página principal
+		response.sendRedirect("Principal");
+	}
+
+}
